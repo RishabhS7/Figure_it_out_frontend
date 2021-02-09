12d87530e77b34aacb7fe90e4b539a4672fb881e
Rishabh Singh <rishabh.katheria@minddeft.net>
2021-02-09 22:42:16 +0530

    Updates

---
+++ precache-manifest.6fefbe02ffc305ea5efdf29648adb7aa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e31896303b1ddce2ebb3e34f8c65fd3a",
+    "url": "/figure-it-0ut/index.html"
+  },
+  {
+    "revision": "e29052f2d35ebcc60420",
+    "url": "/figure-it-0ut/static/css/main.1efda178.chunk.css"
+  },
+  {
+    "revision": "5e0497344beeb6cfe7d9",
+    "url": "/figure-it-0ut/static/js/2.1637bcb9.chunk.js"
+  },
+  {
+    "revision": "3be25bf51018a491ef7f73dfd2dd74c4",
+    "url": "/figure-it-0ut/static/js/2.1637bcb9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e29052f2d35ebcc60420",
+    "url": "/figure-it-0ut/static/js/main.ead8034f.chunk.js"
+  },
+  {
+    "revision": "627d1dbcb565bf083ee5",
+    "url": "/figure-it-0ut/static/js/runtime-main.9d98e19b.js"
+  }
+]);
